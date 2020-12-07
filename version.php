802b4a3942d139c7f9e6873d34c4d7cf9ad6734d
Andrew Nicols <andrew@nicols.co.uk>
2020-12-07 14:10:14 +0800

    Release version 2020120701

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020120700;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2020120701;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2019052000;        // Requires this Moodle version.
 $plugin->component = 'enrol_oneroster'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '2020-12-03';
+$plugin->release = '2020-12-07';
 $plugin->maturity = MATURITY_ALPHA;
