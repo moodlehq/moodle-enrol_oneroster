114390e932bc5ccb1bb340ba5e235e700e2c7cbb
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2023-11-03 14:48:25 +0000

    2023110300

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023031100;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2023110300;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2019052000;        // Requires this Moodle version.
 $plugin->component = 'enrol_oneroster'; // Full name of the plugin (used for diagnostics).
 $plugin->release = '2022-09-19';
